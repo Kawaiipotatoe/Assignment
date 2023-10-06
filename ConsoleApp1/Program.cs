2aed1f6fe02ad7c884d2dde305c1dc280c677aba
Claratatoes <hasnad10@gmail.com>
2023-10-06 23:12:39 +0500

    10 Variablesssss

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,58 +1,87 @@
 ﻿using System;
 namespace ConsoleApp1
-{ 
+{
 
-internal class ConsoleApp1
+    internal class ConsoleApp1
     {
 
-        public static int Add(int a, int b)
-        {int result = a + b; return result;}
-        public static int Subtract(int a, int b)
-        {int result = a - b; return result;}
-        public static int Multiply(int a, int b)
-        {int result = a * b; return result;}
-        public static float Divide(float a, float b)
-        {float result = a / b;return result;}
+        public static void Main(string[] args)
+        {
+
+            //Names
+            string Student1 = "January";
+            string Student2 = "Feb";
+            string Student3 = "March";
+            string Student4 = "April";
+            string Student5 = "May";
+            string Student6 = "June";
+            string Student7 = "July";
+            string Student8 = "August";
+            string Student9 = "Sept";
+            string Student10 = "Oct";
+
+            //Ages
+            int age1 = 20;
+            int age2 = 21;
+            int age3 = 22;
+            int age4 = 23;
+            int age5 = 24;
+            int age6 = 25;
+            int age7 = 26;
+            int age8 = 27;
+            int age9 = 28;
+            int age10 = 29;
+
+            //GPA
+            float GPA1 = 1.1f;
+            float GPA2 = 1.2f;
+            float GPA3 = 1.3f;
+            float GPA4 = 1.5f;
+            float GPA5 = 1.6f;
+            float GPA6 = 1.7f;
+            float GPA7 = 1.8f;
+            float GPA8 = 1.9f;
+            float GPA9 = 2.0f;
+            float GPA10 = 2.1f;
+
+            //PassorFail
+            bool Pass1 = false;
+            bool Pass2 = false;
+            bool Pass3 = false;
+            bool Pass4 = false;
+            bool Pass5 = false;
+            bool Pass6 = false;
+            bool Pass7 = false;
+            bool Pass8 = false;
+            bool Pass9 = true;
+            bool Pass10 = true;
+
+            //Marks
+            char marks1 = 'f';
+            char marks2 = 'f';
+            char marks3 = 'd';
+            char marks4 = 'd';
+            char marks5 = 'd';
+            char marks6 = 'd';
+            char marks7 = 'd';
+            char marks8 = 'd';
+            char marks9 = 'c';
+            char marks10 = 'c';
 
 
-        public static string Text(string a)
-        {string name = a;return name;}
-        public static string Text2(string a)
-        {string name2 = a;return name2;}
-        public static string Text3(string a)
-        {string name3 = a;return name3;}
-        public static string Text4(string a)
-        {string name4 = a;return name4;}
-       
-        public static bool even(int a)
-        {return a % 2 == 0;}
-        public static bool odd(int a)
-        {return a % 2 != 0;}
 
-        public static void Main()
-         {
-            int AddResult = Add(5, 3);
-            Console.WriteLine(AddResult);
-            int SubtractResult = Subtract(5, 3);
-            Console.WriteLine(SubtractResult);
-            float DivideResult = Divide(5, 3);
-            Console.WriteLine(DivideResult);
-            int MultiplyResult = Multiply(5, 3);
-            Console.WriteLine(MultiplyResult);
 
-            string Nameresult = Text("hehe");
-            Console.WriteLine(Nameresult);
-            string Nameresult2 = Text("haha");
-            Console.WriteLine(Nameresult2);
-            string Nameresult3 = Text("huhu");
-            Console.WriteLine(Nameresult3);
-            string Nameresult4 = Text("hoho");
-            Console.WriteLine(Nameresult4);
 
-            bool oddd = odd(5);
-            Console.WriteLine(oddd);
-            bool evenn = even(5);
-            Console.WriteLine(evenn);
+            Console.WriteLine("Name: " + Student1 + "\nAge:" + age1 + "\nGPA:" + GPA1 + "\nPass: " + Pass1 + "\nmarks: " + marks1);
+            Console.WriteLine("\nName: " + Student2 + "\nAge:" + age2 + "\nGPA:" + GPA2 + "\nPass: " + Pass2 + "\nmarks: " + marks2);
+            Console.WriteLine("\nName: " + Student3 + "\nAge:" + age3 + "\nGPA:" + GPA3 + "\nPass: " + Pass3 + "\nmarks: " + marks3);
+            Console.WriteLine("\nName: " + Student4 + "\nAge:" + age4 + "\nGPA:" + GPA4 + "\nPass: " + Pass4 + "\nmarks: " + marks4);
+            Console.WriteLine("\nName: " + Student5 + "\nAge:" + age5 + "\nGPA:" + GPA5 + "\nPass: " + Pass5 + "\nmarks: " + marks5);
+            Console.WriteLine("\nName: " + Student6 + "\nAge:" + age6 + "\nGPA:" + GPA6 + "\nPass: " + Pass6 + "\nmarks: " + marks6);
+            Console.WriteLine("\nName: " + Student7 + "\nAge:" + age7 + "\nGPA:" + GPA7 + "\nPass: " + Pass7 + "\nmarks: " + marks7);
+            Console.WriteLine("\nName: " + Student8 + "\nAge:" + age8 + "\nGPA:" + GPA8 + "\nPass: " + Pass8 + "\nmarks: " + marks8);
+            Console.WriteLine("\nName: " + Student9 + "\nAge:" + age9 + "\nGPA:" + GPA9 + "\nPass: " + Pass9 + "\nmarks: " + marks9);
+            Console.WriteLine("\nName: " + Student10 + "\nAge:" + age10 + "\nGPA:" + GPA10 + "\nPass: " + Pass10 + "\nmarks: " + marks10);
 
         }
     }
